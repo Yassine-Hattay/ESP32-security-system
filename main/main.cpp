68486f0e2beee2d8d3ba0d62551c1f50f1b2450a
Yassine-Hattay <hattayyassine519@gmail.com>
2025-02-08 19:40:03 +0100

    fina test

--- main/main.cpp
+++ main/main.cpp
@@ -1297,7 +1297,7 @@ extern "C" void app_main()
   &loop_handle            
   );
 
-  connected_internet = checkSMTPService(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword) ; //final test2
+  connected_internet = checkSMTPService(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword) ; //final test3
 
   if(connected_internet) // change   if(connected_internet)
 
